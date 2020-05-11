18a149451678ed99fe90f6e48b3fc2c4e9f4f863
Shaun Daley <shaundaley39@gmail.com>
2020-05-11 09:34:24 +0000

    bugfix: twiddle and r

--- stwiddle.py
+++ stwiddle.py
@@ -20,7 +20,7 @@ def twiddle(target_speed=20, p = [0.1, 0.0002, 0.2], r=10, tol=0.01):
     wp = [1 / i for i in p]
     dp = [0.1 * i for i in p]
     frames = 100 + (12000 / target_speed )
-    f = [(r-1) / r, (r+1) / r]
+    f = [(r-1.0) / r, (r+1.0) / r]
     best_err = run(target_speed, frames, p)
     it = 0
     while sum([i*j for i,j in zip(wp,dp)]) > tol:
@@ -50,7 +50,7 @@ giving_up = 0
 
 while(giving_up < 4):
   print("# Speed: {} mph".format(target_speed))
-  p, best_err = twiddle(target_speed=20, p = p)
+  p, best_err = twiddle(target_speed=target_speed, p=p)
   print("Speed: {}, attained a lowest penalty of {} with parameters {}".format(target_speed, best_err, p))
   if (best_err < 5000.0):
     target_speed += d_speed
